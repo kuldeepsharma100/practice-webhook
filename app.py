87a87d7ad16cffb28ea17f0891e9580bbe2e81ac
kuldeepsharma100 <149848065+kuldeepsharma100@users.noreply.github.com>
2025-11-28 09:39:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from Flask app Practice done and last and final update again testing mail is having problem"
+    return "Hello from Flask app Practice done and last and final update again testing mail is having problem one more"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
